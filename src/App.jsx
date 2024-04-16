943c1555f4cafde209eb3dff8680d59d5b474fe5
AdriPad99 <adrian.mpadilla99@gmail.com>
2024-04-16 17:37:08 -0500

    last minute

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import NavBar from "./components/NavBar"
 import SignUp from "./components/SignUp"
 import SignIn from "./components/SignIn"
+import LandingPage from "./components/LandingPage"
 
 export default function App() {
   return (
     <>
       <NavBar/>
+
+      <LandingPage/>
 
       <SignUp/>
 
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function LandingPage() {
+  return (
+    <>
+    <div id='land-div'>
+        <h1>Welcome to the bootleg Coding Temple!</h1>
+        <h3>Please sign in to access the specialized content!</h3>
+        sign-in / sign-up functionality not currently working :(
+    </div>
+    </>
+  )
+}
