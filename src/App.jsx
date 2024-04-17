5ca4aaf49434abd424f9e2147eb05f58fef98880
AdriPad99 <adrian.mpadilla99@gmail.com>
2024-04-16 21:08:07 -0500

    state 1 finish

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import NavBar from "./components/NavBar"
 import SignUp from "./components/SignUp"
 import SignIn from "./components/SignIn"
 import LandingPage from "./components/LandingPage"
+import GameClassPicker from "./components/GameClassPicker"
 
 export default function App() {
   return (
     <>
       <NavBar/>
 
-      <LandingPage/>
+      <GameClassPicker/>
 
       <SignUp/>
 
---
+++ src/components/GameClassPicker.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+import React from 'react'
+
+
+export default function GameClassPicker() {
+
+    let classes = ['Monk', 'Rogue', 'Mage', 'Archer', 'Ranger', 'Berserker', 'Sorcerer',
+    'Cleric', 'Bard', 'Thief', 'Assassin', 'Alchemist', 'Fighter', 'Wizard', 'Ninja']
+
+    const[userIdx, setUserIdx] = useState(0);
+
+    const prevClass = () => {
+        if(userIdx === 0){
+        }
+        else {
+            setUserIdx(userIdx - 1)
+        }
+    }
+
+    const nextClass = () => {
+        if(userIdx === classes.length -1) {
+        }
+        else {
+            setUserIdx(userIdx + 1)
+        }
+    }
+
+
+  return (
+    <>
+    <div id='game-div'>
+        <div>
+            <h3>Choose your character class!</h3>
+        </div>
+        <div>
+            <button id='class-btn' onClick={prevClass}>Previous Class</button>
+            {classes[userIdx]}
+            <button id='class-btn' onClick={nextClass}>Previous Class</button>
+        </div>
+    </div>
+    <br/>
+    </>
+  )
+}
